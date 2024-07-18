01dd4ad3146edff88cc4c851675afa5aff7879eb
TSB <122724396+tsbproject@users.noreply.github.com>
2024-07-18 15:42:45 +0100

    [committed]

--- js/script.js
+++ js/script.js
@@ -1,3 +1,42 @@
+
+document.addEventListener('DOMContentLoaded', function() {
+    const navbar = document.getElementById('navbar');
+    const logo = document.getElementById('logo');
+    const navlink = document.getElementById('navlink');
+    let lastScrollTop = 0;
+
+    window.addEventListener('scroll', function() {
+        let scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+
+        if (scrollTop > lastScrollTop) {
+            // Scrolling down
+            navbar.classList.add('shrink');
+            logo.classList.add('shrink');
+            navlink.classList.add('shrink');
+        } else {
+            // Scrolling up
+            if (scrollTop === 0) {
+                navbar.classList.remove('shrink');
+                logo.classList.remove('shrink');
+                navlink.classList.remove('shrink');
+            }
+        }
+
+        lastScrollTop = scrollTop;
+    });
+});
+
+
+
+
+
+
+
+
+
+
+
+
 // THIS HIDING HTML EXTENTION IN BROWSER
 if (window.history && window.history.pushState) {
     window.history.pushState({}, document.title, window.location.pathname.replace('.html', ''));
